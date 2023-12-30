19a07fdc3cec1769d97fd67dab34fd53942674df
Pavan Gujjari <pavan.gujjari09@gmail.com>
2023-12-30 20:49:27 +0530

    Add files via upload
    Headers and footers

---
+++ config/footer.php
@@ -0,0 +1,53 @@
+<!-- Footer Page -->  
+<footer class="page-footer bg-dark mt-4">
+	<div class="">
+		<div class="container">
+			<div class="row py-4 d-flex align-items-center">
+			
+				<div class="col-md-12 text-center">
+					<a href="#"><i class="fab fa-facebook-f text-light mr-4"></i></a>
+					<a href="#"><i class="fab fa-twitter text-light mr-4"></i></a>
+					<a href="#"><i class="fab fa-google-plus-g text-light mr-4"></i></a>
+					<a href="#"><i class="fab fa-linkedin-in text-light mr-4"></i></a>
+					<a href="#"><i class="fab fa-instagram text-light mr-4"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container text-center text-md-left mt-5">
+		<div class="row">
+			<div class="col-md-4 col-sm-4 text-center text-light">
+				<h6> Quick links</h6>
+				<hr class="bg mb-4 mt-0-d-inline-block mx-auto" style="width:85px; height: 2px;">
+				<ul class="list-unstyled">
+					<li class="my-2"><a class="links" href="../index.php">Home</a></li>
+					<li class="my-2"><a class="links" href="../navbar/Register.php">Join us</a></li>
+					<li class="my-2"><a class="links" href="../navbar/events.php">events</a></li>
+				</ul>
+			</div>
+			<div class="col-md-4 col-sm-4 text-center text-light">
+				<h6>Navigation </h6>
+				<hr class="bg mb-4 mt-0-d-inline-block mx-auto" style="width:85px; height: 2px;">
+				<ul class="list-unstyled">
+					<li class="my-2"><a class="links" href="../navbar/blog.php">blogs</a></li>
+					<li class="my-2"><a class="links" href="../navbar/events.php">Gallery</a></li>
+				</ul>
+			</div>
+			<div class="col-md-4 col-sm-4 text-center text-light">
+				<h6>Contact Us</h6>
+				<hr class="bg mb-4 mt-0-d-inline-block mx-auto" style="width:85px; height: 2px;">
+				<ul class="list-unstyled">
+					<li class="my-2"><i class="fas fa-phone-alt bg-dark mr-2"></i> <a class="links" href="#"> 	9912345678</a></li>
+					<li class="my-2"><i class="fas fa-envelope bg-dark mr-2"></i><a class="links" href="#">infogathering@pavangujjari.com</a></li>
+					<li class="my-2"><i class="fas fa-map-marked-alt bg-dark mr-2"></i><a class="links" href="#">Hyderabad</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<hr>
+		<div class="border-top border-success w-25 mx-auto my-3"></div>
+		<h5 class="text-center pb-4 text-light font">Farmers: <span class="light-text">We feed the world</span></h5>
+	</div>
+</footer>
